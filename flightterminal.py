e667ac7dc50865cf1d3b427aea7efe062df9bf13
KK4TEE <kk4tee@server.fake>
2014-02-24 17:17:05 -0500

    Combined the arduino handler into flight terminal

--- flightterminal.py
+++ flightterminal.py
@@ -8,10 +8,32 @@ import math
 import time
 import curses
 import telemachus_plugin as tele
+import config
+
+try:
+    import serial
+except:
+    print 'PySerial does not seem to be installed'
+
+try:
+    ser = serial.Serial(
+        port=config.arduinoSerialPort(),
+        #port='COM3',
+        #baudrate=115200, # Causes the arduino buffer to fill up
+        baudrate=9600, # Seems to be working well
+       # parity=serial.PARITY_ODD,
+       # stopbits=serial.STOPBITS_TWO,
+       # bytesize=serial.SEVENBITS
+    )
+    arduinoConnected = True
+    print 'Serial device connected at ' + config.arduinoSerialPort()
+except:
+    arduinoConnected = False
+    print 'Unable to connect to arduino at ' + config.arduinoSerialPort()
 
 
 def getFlightData(d):
-    d['MET'] = int(tele.read_missiontime())
+    d['MET'] = float(tele.read_missiontime())
     d['ASL'] = int(tele.read_asl())
     d['Ap'] = int(tele.read_apoapsis())
     d['Pe'] = int(tele.read_periapsis())
@@ -19,7 +41,9 @@ def getFlightData(d):
     d['Time to Pe'] = float(tele.read_time_to_pe())
     d['Eccentricity'] = float(tele.read_eccentricity())
     d['Inclination'] = float(tele.read_inclination())
+    time.sleep(0.01)
     d['Orbital Period'] = float(tele.read_orbitalperiod())
+    d['Vertical Speed'] = float(tele.read_verticalspeed())
 
     d['SAS Status'] = int(tele.sas(2))
     d['RCS Status'] = int(tele.rcs(2))
@@ -27,13 +51,15 @@ def getFlightData(d):
 
     d['ElectricCharge'] = float(tele.read_resource('ElectricCharge'))
     d['Max ElectricCharge'] = float(tele.read_resource_max('ElectricCharge'))
+    time.sleep(0.01)
     d['LiquidFuel'] = float(tele.read_resource('LiquidFuel'))
     d['Max LiquidFuel'] = float(tele.read_resource_max('LiquidFuel'))
     d['Oxidizer'] = float(tele.read_resource('Oxidizer'))
     d['MaxOxidizer'] = float(tele.read_resource_max('Oxidizer'))
     d['SolidFuel'] = float(tele.read_resource('SolidFuel'))
     d['Max SolidFuel'] = float(tele.read_resource_max('SolidFuel'))
-
+    # I need to add handling for losing contact with the server. It's good
+    # practice plus should help with the HTTP 500 errors
     return d
 
 
@@ -79,6 +105,81 @@ def drawVGauge(gLabel, percentVal, yCord, xCord):
         gauge.addstr(23, 6, 'Empty')
 
 
+###  Arduino Utilities
+
+
+
+def push_to_arduino(inputline):
+    #ser.flushOutput()
+    # Send data to the Arduino and end w/ a newline
+    ser.write(inputline + '\n')
+    #ser.write("255, 255, 255 \n")
+    #time.sleep(.2)
+
+
+def buttonHandler():
+    global gearStatus
+    global brakeStatus
+    global memB
+    global memBOLD
+    if memB[1] == '1' and memB[1] != memBOLD[1]:
+        if (memB[7] == '1'):  # Check the safety
+            tele.stage()
+
+    if memB[0] == '1' and memB[0] != memBOLD[0]:
+        if memB[7] == '1':  # Check the safety
+            tele.abort()
+
+    if int(memB[2]) == 1 and memB[2] != memBOLD[2]:
+        # Toggle gear based on what we did last time
+        if gearStatus == 1:  # Telemachus does not yet read gear status
+            tele.gear(0)
+            gearStatus = 0
+        elif gearStatus == 0:
+            tele.gear(1)
+            gearStatus = 1
+
+    if int(memB[3]) == 1 and memB[3] != memBOLD[3]:
+        # Toggle Light based on the Telemachus reading
+        if tele.light(2) == 1:
+            tele.light(0)
+        elif tele.light(2) == 0:
+            tele.light(1)
+
+    if int(memB[4]) == 1 and memB[4] != memBOLD[4]:
+        # Toggle brake based on what we did last time
+        if brakeStatus == 1:  # Telemachus does not yet read brake status
+            tele.brake(0)
+            brakeStatus = 0
+        elif brakeStatus == 0:
+            tele.brake(1)
+            brakeStatus = 1
+
+    if int(memB[5]) == 1 and memB[5] != memBOLD[5]:
+        # Toggle RCS based on the Telemachus reading
+        if tele.rcs(2) == 1:
+            tele.rcs(0)
+        elif tele.rcs(2) == 0:
+            tele.rcs(1)
+
+    if int(memB[6]) == 1 and memB[6] != memBOLD[6]:
+        # Toggle SAS based on the Telemachus reading
+        if tele.sas(2) == 1:
+            tele.sas(0)
+        elif tele.sas(2) == 0:
+            tele.sas(1)
+
+
+def clamp(num, minn, maxn):
+    if num < minn:
+        return minn
+    elif num > maxn:
+        return maxn
+    else:
+        return num
+
+
+#### Main Program ############################################################
 myscreen = curses.initscr()
 if curses.can_change_color:
     curses.start_color()
@@ -97,10 +198,20 @@ chrin = -1
 filldigits = 8
 arduinoActive = 0
 
-# Flight Data Memory
-fd = {'zero': 0}
+# Flight Data Memory and other variables
+fd = {'zero': 0}  # Primary data storage
+arduinoSleepMarker = 0
+buttonSleepMarker = 0
+gearStatus = 0
+brakeStatus = 0
+memB = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # Current serial input
+memBOLD = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # Old serial input
+n = 0
 
+### Flight Computer Section ##################################################
 while chrin != 48:
+    loopStartTime = time.time()
+    myscreen.clear()
     fd = getFlightData(fd)
     myscreen.border()
     maxY, maxX = myscreen.getmaxyx()
@@ -110,7 +221,7 @@ while chrin != 48:
     #
     myscreen.addstr(yLine, 1, "##     Mission Time:     ##", curses.A_STANDOUT)
     yLine += 1
-    myscreen.addstr(yLine, 4, str(fd['MET'])
+    myscreen.addstr(yLine, 4, str(round(fd['MET'], 1))
         .zfill(21), curses.color_pair(1))
     yLine += 1
     yLine += 1
@@ -204,21 +315,105 @@ while chrin != 48:
     else:
         drawVGauge("Solid Fuel", 0, 1, 65)
 
-    if chrin == 49:  # If user presses '1', toggle arduino bit
+### Arduino Section ##########################################################
+    if arduinoConnected is True:
+        if chrin == 49:  # If user presses '1', toggle arduino bit
+            if arduinoActive == 0:
+                arduinoActive = 1
+            elif arduinoActive == 1:
+                arduinoActive = 0
+
         if arduinoActive == 0:
-            arduinoActive = 1
+            myscreen.addstr(maxY - 1, 2, " Press 1 to Activate Arduino ")
         elif arduinoActive == 1:
-            arduinoActive = 0
+            myscreen.addstr(maxY - 1, 2, " Press 1 to Deactivate Arduino ")
+    else:
+        myscreen.addstr(maxY - 1, 2, " Arduino not available ")
 
-    if arduinoActive == 0:
-        myscreen.addstr(maxY - 1, 2, " Press 1 to Activate Arduino ")
-    elif arduinoActive == 1:
-        myscreen.addstr(maxY - 1, 2, " Press 1 to DeActivate Arduino ")
 
+    if arduinoActive == 1:
+        climbgauge = tele.read_verticalspeed()
+        if climbgauge > 0:
+            climbgauge = clamp((int(4 * math.sqrt(climbgauge)) + 127), 0, 2255)
+        elif climbgauge < 0:
+            climbgauge = clamp((0 - int(4 * math.sqrt(
+                abs(climbgauge))) + 127), 0, 255)
+        else:
+            climbgauge = 127  # Neutral
+
+        memA = (str(int(round(fd["MET"]))).zfill(8)
+            + str(int(round(fd["ASL"] / 100))).zfill(8)
+            + str(int(round(fd["Ap"] / 100))).zfill(8)
+            + str(int(round(fd["Pe"] / 100))).zfill(8)
+            + str(int(round(fd['Vertical Speed']))).zfill(8)
+            + chr(climbgauge) + 'BCDEFGH'
+            )
+
+        #arduinostring = [255, 255, 255]
+
+        if arduinoSleepMarker > 0.2:
+            try:
+                push_to_arduino(memA)
+            finally:
+                arduinoSleepMarker = 0
+
+        if ser.inWaiting > 9:
+            serCharIn = str(ser.read(1))
+            if serCharIn == '[':
+                while n < 12:
+                    serCharIn = str(ser.read(1))
+                    if serCharIn == ']':
+                        n = 0
+                        ser.flushInput()
+                        break
+                    else:
+                        memB[n] = serCharIn
+                    n += 1
+                    if n == 11:
+                        ser.flushInput()
+
+        if buttonSleepMarker > 0.1:
+            buttonHandler()  # Reads memB for which buttons are pressed, then sends
+                             # calls to telemachus as needed.
+            button_sleep_marker = 0
+            memBOLD = list(memB)
+
+        myscreen.addstr(yLine, 1, "##   Arduino Readouts    ##",
+         curses.A_STANDOUT)
+        yLine += 1
+        myscreen.addstr(yLine, 1, "memA:")
+        myscreen.addstr(yLine, 20, str(memA))
+        yLine += 1
+        myscreen.addstr(yLine, 1, "memB:")
+        myscreen.addstr(yLine, 20, str(memB))
+        yLine += 1
+        myscreen.addstr(yLine, 1, "Program Loop Time:")
+        myscreen.addstr(yLine, 20, str(round(loopTime, 5)))
+        yLine += 1
+        yLine += 1
+
+
+
+
+
+
+
+
+### Main Loop Cleanup ########################################################
     myscreen.addstr(maxY - 1, maxX - 19, " Press 0 to Exit ")
     myscreen.refresh()
     chrin = myscreen.getch()
-    time.sleep(0.25)
+
+    loopTimeOffset = 0.25 + loopStartTime - time.time()
+    if loopTimeOffset > 0:
+        time.sleep(loopTimeOffset)
+    # Combined Bandwidth used based on interval:
+    # 33ms = around 395 Packets/S, 345kbs
+    # 25ms = around 500 Packets/S, 410kbs
+    loopEndTime = time.time()
+    loopTime = loopEndTime - loopStartTime
+    arduinoSleepMarker += loopTime
+    buttonSleepMarker += loopTime
 
 
 curses.nocbreak()
