b31d142087b9f28ae9d073252df013cde5bb6b83
KK4TEE <kk4tee@server.fake>
2014-03-02 19:08:44 -0500

    Rewrote config file to only pass arguments instead of loading modules

--- config.py
+++ config.py
@@ -1,18 +1,16 @@
 #Config File for JSON Proccessor and the python/telemachus library
-import serial
 
-url = 'http://127.0.0.1:8085/telemachus/datalink?alt='
-#url = 'http://192.168.1.3:8085/telemachus/datalink?alt='
+def url():
+    #return url = 'http://127.0.0.1:8085/telemachus/datalink?alt='
+    return 'http://192.168.1.2:8085/telemachus/datalink?alt='
     # This is the URL that Telemachus can be found at.
     # Adjust it based on your firewall settings.
 
 
-ser = serial.Serial(
-    port='/dev/ttyACM0',
-    #port='COM3',
-    #baudrate=115200, # Causes the arduino buffer to fill up
-    baudrate=9600, # Seems to be working well
-   # parity=serial.PARITY_ODD,
-   # stopbits=serial.STOPBITS_TWO,
-   # bytesize=serial.SEVENBITS
-)
+def arduinoSerialPort():
+    return '/dev/ttyACM0'
+
+def pollInterval():
+    # How often should the program poll the game for flight data?
+    return 0.20
+
